d9fd778ca726d17d68bcb5c091948752732a317a
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2017-06-24 22:08:38 +0200

    Started adding an example; extended bindings; started adding parameter names to bindings.

--- src/whitedb.cr
+++ src/whitedb.cr
@@ -1,5 +1,25 @@
 require "./whitedb/*"
 
-module Whitedb
-  # TODO Put your code here
-end
+# Connecting to the database
+db : Void*
+rec : Void*; rec2 : Void*
+
+db = WhiteDB.attach_database "dick", 200_000
+
+# Adding data to the database
+## Records
+rec = WhiteDB.create_record(db, 10)
+rec2 = WhiteDB.create_record(db, 2)
+
+# Data in WhiteDB
+enc = WhiteDB.encode_int(db, 443)
+enc2 = WhiteDB.encode_str(db, "this is my string", nil)
+
+## Setting field values
+WhiteDB.set_field(db, rec, 7, enc)
+WhiteDB.set_field(db, rec2, 0, enc2)
+
+# Making Requests
+## Finding matching records
+rec = WhiteDB.find_record_int(db, 7, WhiteDB::COND::EQUAL, 443, nil)
+
--- src/whitedb/bindings.cr
+++ src/whitedb/bindings.cr
@@ -1,4 +1,4 @@
-@[Link("whitedb/dbapi.h")]
+@[Link("wgdb")]
 lib WhiteDB
   alias Int = Int64
   alias Char = LibC::Char
@@ -10,19 +10,22 @@ lib WhiteDB
   alias Short = LibC::Short
   alias Int64T = LibC::Int64T
 
-  fun attach_database = wg_attach_database(Char*, Int) : Void*
+  alias String = LibC::Char*
+
+
+  fun attach_database = wg_attach_database(dbasename : String, size : Int) : Void*
   fun attach_exisitng_database = wg_attach_exisitng_database(Char*) : Void*
   fun attach_logged_database = wg_attach_logged_database(Char*, Int, Int) : Void*
   fun attach_database_mode = wg_attach_database_mode(Char*, Int, Int) : Void*
   fun detach_database = wg_detach_database(Void*) : Int
   fun delete_database = wg_delete_database(Char*) : Int
   
-  
+
   fun attach_local_database = wg_attach_local_database(Int) : Void*
-  fun delete_local_database = wg_delete_local_database(Void*) : Void
+  fun delete_local_database = wg_delete_local_database(db : Void*) : Void
 
   
-  fun create_record = wg_create_record(Void*, Int) : Void*
+  fun create_record = wg_create_record(db : Void*, length : Int) : Void*
   fun create_raw_record = wg_create_raw_record(Void*, Int) : Void*
   fun delete_record = wg_delete_record(Void*, Void*) : Int
   fun get_first_record = wg_get_first_record(Void*) : Void*
@@ -33,7 +36,7 @@ lib WhiteDB
   
   fun get_record_len = wg_get_record_len(Void*, Void*) : Int
   
-  fun set_field = wg_set_field(Void*, Void*, Int, Int) : Int
+  fun set_field = wg_set_field(db : Void*, record_ : Void*, fieldnr : Int, data : Int) : Int
   fun set_new_field = wg_set_new_field(Void*, Void*, Int, Int) : Int
   
   fun get_field = wg_get_field(Void*, Void*, Int) : Int
@@ -68,8 +71,8 @@ lib WhiteDB
   fun encode_null = wg_encode_null(Void*, Int) : Int
   fun decode_null = wg_decode_null(Void*, Int) : Int
 
-  fun encode_int = wg_encode_int(Void*, Int) : Int
-  fun decode_int = wg_decode_int(Void*, Int) : Int
+  fun encode_int = wg_encode_int(int : Void*, data : Int) : Int
+  fun decode_int = wg_decode_int(int : Void*, data : Int) : Int
 
   fun encode_char = wg_encode_char(Void*, Char) : Int
   fun decode_char = wg_decode_char(Void*, Int) : Char
@@ -103,7 +106,7 @@ lib WhiteDB
   fun date_to_ymd = wg_date_to_ymd(Void*, Int, Int*, Int*, Int*) : Void*
   fun time_to_hms = wg_time_to_hms(Void*, Int, Int*, Int*, Int*, Int*) : Void*
 
-  fun encode_str = wg_encode_str(Void*, Char*, Char*) : Int
+  fun encode_str = wg_encode_str(db : Void*, str : Char*, lang : Char*) : Int
 
   fun decode_str = wg_decode_str(Void*, Int) : Char*
   fun decode_str_lang = wg_decode_str_lang(Void*, Int) : Char*
@@ -170,4 +173,40 @@ lib WhiteDB
   fun print_record = wg_print_record(Void*, Int*) : Void
   fun snprint_value = wg_snprint_value(Void*, Int*) : Void
   fun parse_and_encode = wg_parse_and_encode(Void*, Char*) : Void
+
+  # ...
+
+  # Simplyfied query functions
+  fun find_record = wg_find_record(db : Void*, fieldnr : Int, cond : Int, data : Int, lastrecord : Void*) : Void*
+  fun find_record_null = wg_find_record_null(db : Void*, fieldnr : Int, cond : Int, data : String, lastrecord : Void*) : Void*
+  fun find_record_record = wg_find_record_record(db : Void*, fieldnr : Int, cond : Int, data : Void*, lastrecord : Void*) : Void*
+  fun find_record_char = wg_find_record_char(db : Void*, fieldnr : Int, cond : Int, data : Char, lastrecord : Void*) : Void*
+  fun find_record_fixpoint = wg_find_record_fixpoint(db : Void*, fieldnr : Int, cond : Int, data : Double, lastrecord : Void*) : Void*
+  fun find_record_date = wg_find_record_date(db : Void*, fieldnr : Int, cond : Int, data : Int, lastrecord : Void*) : Void*
+  fun find_record_time = wg_find_record_time(db : Void*, fieldnr : Int, cond : Int, data : Int, lastrecord : Void*) : Void*
+  fun find_record_var = wg_find_record_var(db : Void*, fieldnr : Int, cond : Int, data : Int, lastrecord : Void*) : Void*
+  fun find_record_int = wg_find_record_int(db : Void*, fieldnr : Int, cond : Int, data : Int, lastrecord : Void*) : Void*
+  fun find_record_double = wg_find_record_double(db : Void*, fieldnr : Int, cond : Int, data : Double, lastrecord : Void*) : Void*
+  fun find_record_str = wg_find_record_str(db : Void*, fieldnr : Int, cond : Int, data : String, lastrecord : Void*) : Void*
+  fun find_record_xmlliteral = wg_find_record_xmlliteral(db : Void*, fieldnr : Int, cond : Int, data : String, xsdtype : String, lastrecord : Void*) : Void*
+  fun find_record_uri = wg_find_record_uri(db : Void*, fieldnr : Int, cond : Int, data : String, prefix : String, lastrecord : Void*) : Void*
+
+  # ...
+  
+
+  fun database_freesize = wg_database_freesize(db : Void*) : Int
+  fun database_size = wg_database_size(db : Void*) : Int
+
+  
+  # ...
+
+  # Query "arglist" parameters
+  enum COND
+    EQUAL     = 1
+    NOT_EQUAL = 1 << 1
+    LESSTHAN  = 1 << 2
+    GREATER   = 1 << 3
+    LTEQUAL   = 1 << 4
+    GTEQUAL   = 1 << 5 
+  end
 end
