9ee6eac232f03bdde109c5b4279cee4a2bf9d115
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2017-06-25 16:19:46 +0200

    (Almost) finished adding the whitedb tutorial (and its bindings)

--- src/whitedb.cr
+++ src/whitedb.cr
@@ -1,10 +1,13 @@
 require "./whitedb/*"
 
-# Connecting to the database
-db : Void*
-rec : Void*; rec2 : Void*
+#> Examples taken from: https://whitebd.org/tutorial
 
-db = WhiteDB.attach_database "dick", 200_000
+# db : Void*
+# rec : Void*; rec2 : Void*
+# enc : Void*; enc2 : Void*
+
+# Connecting to the database
+db = WhiteDB.attach_database "dbname", 200_000
 
 # Adding data to the database
 ## Records
@@ -21,5 +24,34 @@ WhiteDB.set_field(db, rec2, 0, enc2)
 
 # Making Requests
 ## Finding matching records
+### -> "field 7 equals 443"
 rec = WhiteDB.find_record_int(db, 7, WhiteDB::COND::EQUAL, 443, nil)
+while rec
+  puts "Found a record where field 7 is 443"
+  rec = WhiteDB.find_record_int(db, 7, WhiteDB::COND::EQUAL, 443, rec);
+end
 
+## Full query interface
+arglist = [] of WhiteDB::QueryArg
+arglist[0].column = 7
+arglist[0].cond = WhiteDB::COND::EQUAL
+arglist[0].value = WhiteDB.encode_query_param_int(db, 443)
+
+arglist[1].column = 6
+arglist[1].cond = WhiteDB::COND::EQUAL
+arglist[1].value = WhiteDB.encode_query_param_null(db, nil)
+
+# -> "field 7 equals 443 and field 6 equals NULL"
+query = WhiteDB.make_query(db, nil, 0, arglist, 2) #< arglist should be a pointer (of queryargs)
+
+while rec = WhiteDB.fetch(db, query)
+  puts "Found a record where field 7 is 443 and field 6 is NULL"
+end
+
+# Free the memory allocated for the query
+WhiteDB.free_query(db, query)
+WhiteDB.free_query_param(db, arglist[0].value)
+WhiteDB.free_query_param(db, arglist[1].value)
+
+print "Press enter to continue..."; gets
+WhiteDB.delete_database("dbname")
--- src/whitedb/bindings.cr
+++ src/whitedb/bindings.cr
@@ -14,11 +14,11 @@ lib WhiteDB
 
 
   fun attach_database = wg_attach_database(dbasename : String, size : Int) : Void*
-  fun attach_exisitng_database = wg_attach_exisitng_database(Char*) : Void*
-  fun attach_logged_database = wg_attach_logged_database(Char*, Int, Int) : Void*
-  fun attach_database_mode = wg_attach_database_mode(Char*, Int, Int) : Void*
-  fun detach_database = wg_detach_database(Void*) : Int
-  fun delete_database = wg_delete_database(Char*) : Int
+  fun attach_exisitng_database = wg_attach_exisitng_database(String) : Void*
+  fun attach_logged_database = wg_attach_logged_database(String, Int, Int) : Void*
+  fun attach_database_mode = wg_attach_database_mode(String, Int, Int) : Void*
+  fun detach_database = wg_detach_database(dbase : Void*) : Int
+  fun delete_database = wg_delete_database(dbasename : String) : Int
   
 
   fun attach_local_database = wg_attach_local_database(Int) : Void*
@@ -44,7 +44,7 @@ lib WhiteDB
   
   fun set_int_field = wg_set_int_field(Void*, Void*, Int, Int) : Int
   fun set_double_field = wg_set_double_field(Void*, Void*, Int, Double) : Int
-  fun set_str_field = wg_set_str_field(Void*, Void*, Int, Char*) : Int
+  fun set_str_field = wg_set_str_field(Void*, Void*, Int, String) : Int
   
   fun field_arr = wg_field_arr(Void*, Void*, Int) : Int
 
@@ -97,58 +97,58 @@ lib WhiteDB
   fun wg_current_utctime(Void*) : Int
   fun wg_current_localtime(Void*) : Int
 
-  fun wg_strf_iso_datetime(Void*, Int, Int, Char*) : Int
-  fun wg_strp_iso_date(Void*, Char*) : Int
-  fun wg_strp_iso_time(Void*, Char*) : Int
+  fun wg_strf_iso_datetime(Void*, Int, Int, String) : Int
+  fun wg_strp_iso_date(Void*, String) : Int
+  fun wg_strp_iso_time(Void*, String) : Int
 
   fun wg_ymd_to_date(Void*, Int, Int, Int) : Int
   fun wg_hms_to_time(Void*, Int, Int, Int, Int) : Int
   fun date_to_ymd = wg_date_to_ymd(Void*, Int, Int*, Int*, Int*) : Void*
   fun time_to_hms = wg_time_to_hms(Void*, Int, Int*, Int*, Int*, Int*) : Void*
 
-  fun encode_str = wg_encode_str(db : Void*, str : Char*, lang : Char*) : Int
+  fun encode_str = wg_encode_str(db : Void*, str : String, lang : String) : Int
 
-  fun decode_str = wg_decode_str(Void*, Int) : Char*
-  fun decode_str_lang = wg_decode_str_lang(Void*, Int) : Char*
+  fun decode_str = wg_decode_str(Void*, Int) : String
+  fun decode_str_lang = wg_decode_str_lang(Void*, Int) : String
 
   fun decode_str_len = wg_decode_str_len(Void*, Int) : Int
   fun decode_str_lang_len = wg_decode_str_lang_len(Void*, Int) : Int
-  fun decode_str_copy = wg_decode_str_copy(Void*, Int, Char*, Int) : Int
-  fun decode_str_lang_copy = wg_decode_str_lang_copy(Void*, Int, Char*, Int) : Int
+  fun decode_str_copy = wg_decode_str_copy(Void*, Int, String, Int) : Int
+  fun decode_str_lang_copy = wg_decode_str_lang_copy(Void*, Int, String, Int) : Int
 
-  fun encode_xmlliteral = wg_encode_xmlliteral(Void*, Char*, Char*) : Int
-  fun decode_xmlliteral_copy = wg_decode_xmlliteral_copy(Void*, Int) : Char*
-  fun decode_xmlliteral_xsdtype_copy = wg_decode_xmlliteral_xsdtype_copy(Void*, Int) : Char*
+  fun encode_xmlliteral = wg_encode_xmlliteral(Void*, String, String) : Int
+  fun decode_xmlliteral_copy = wg_decode_xmlliteral_copy(Void*, Int) : String
+  fun decode_xmlliteral_xsdtype_copy = wg_decode_xmlliteral_xsdtype_copy(Void*, Int) : String
 
   fun decode_xmlliteral_len = wg_decode_xmlliteral_len(Void*, Int) : Int
   fun decode_xmlliteral_xsdtype_len = wg_decode_xmlliteral_xsdtype_len(Void*, Int) : Int
-  fun decode_xmlliteral = wg_decode_xmlliteral(Void*, Int, Char*, Int) : Int
-  fun decode_xmlliteral_xsdtype = wg_decode_xmlliteral_xsdtype(Void*, Int, Char*, Int) : Int
+  fun decode_xmlliteral = wg_decode_xmlliteral(Void*, Int, String, Int) : Int
+  fun decode_xmlliteral_xsdtype = wg_decode_xmlliteral_xsdtype(Void*, Int, String, Int) : Int
 
-  fun encode_uri = wg_encode_uri(Void*, Char*, Char*) : Int
-  fun decode_uri = wg_decode_uri(Void*, Int) : Char*
-  fun decode_uri_prefix = wg_decode_uri_prefix(Void*, Int) : Char*
+  fun encode_uri = wg_encode_uri(Void*, String, String) : Int
+  fun decode_uri = wg_decode_uri(Void*, Int) : String
+  fun decode_uri_prefix = wg_decode_uri_prefix(Void*, Int) : String
 
   fun decode_uri_len = wg_decode_uri_len(Void*, Int) : Int
   fun decode_uri_prefix_len = wg_decode_uri_prefix_len(Void*, Int) : Int
-  fun decode_uri_copy = wg_decode_uri_copy(Void*, Int, Char*, Int) : Int
-  fun decode_uri_prefix_copy = wg_decode_uri_prefix_copy(Void*, Int, Char*, Int) : Int
+  fun decode_uri_copy = wg_decode_uri_copy(Void*, Int, String, Int) : Int
+  fun decode_uri_prefix_copy = wg_decode_uri_prefix_copy(Void*, Int, String, Int) : Int
 
-  fun encode_blob = wg_encode_blob(Void*, Char*, Char*, Int) : Int
-  fun decode_blob = wg_decode_blob(Void*, Int) : Char*
-  fun decode_blob_type = wg_decode_blob_type(Void*, Int) : Char*
+  fun encode_blob = wg_encode_blob(Void*, String, String, Int) : Int
+  fun decode_blob = wg_decode_blob(Void*, Int) : String
+  fun decode_blob_type = wg_decode_blob_type(Void*, Int) : String
   fun decode_blob_len = wg_decode_blob_len(Void*, Int) : Int
-  fun decode_blob_copy = wg_decode_blob_copy(Void*, Int, Char*, Int) : Int
+  fun decode_blob_copy = wg_decode_blob_copy(Void*, Int, String, Int) : Int
   fun decode_blob_type_len = wg_decode_blob_type_len(Void*, Int) : Int
-  fun decode_blob_type_copy = wg_decode_blob_type_copy(Void*, Int, Char*, Int) : Int
+  fun decode_blob_type_copy = wg_decode_blob_type_copy(Void*, Int, String, Int) : Int
 
 
   fun encode_var = wg_encode_var(Void*, Int) : Int
   fun decode_var = wg_decode_var(Void*, Int) : Int
 
   
-  fun dump = wg_dump(Void*, Char*) : Int
-  fun import = wg_import(Void*, Char*) : Int
+  fun dump = wg_dump(Void*, String) : Int
+  fun import = wg_import(Void*, String) : Int
   
   fun start_logging = wg_start_logging(Void*) : Int
   fun stop_logging = wg_stop_logging(Void*) : Int
@@ -166,17 +166,59 @@ lib WhiteDB
   
 
   fun parse_json_file = wg_parse_json_file(Void*, Char) : Int
-  fun parse_json_document = wg_parse_json_document(Void*, Char*, Void**) : Int
-  fun parse_json_fragment = wg_parse_json_fragment(Void*, Char*, Void**) : Int
+  fun parse_json_document = wg_parse_json_document(Void*, String, Void**) : Int
+  fun parse_json_fragment = wg_parse_json_fragment(Void*, String, Void**) : Int
   
   fun print_db = wg_print_db(Void*) : Void
   fun print_record = wg_print_record(Void*, Int*) : Void
   fun snprint_value = wg_snprint_value(Void*, Int*) : Void
-  fun parse_and_encode = wg_parse_and_encode(Void*, Char*) : Void
+  fun parse_and_encode = wg_parse_and_encode(Void*, String) : Void
 
   # ...
 
-  # Simplyfied query functions
+  fun database_freesize = wg_database_freesize(db : Void*) : Int
+  fun database_size = wg_database_size(db : Void*) : Int
+  
+  # ...
+
+  # Qeury functions
+
+  alias Query = Void*
+  struct QueryArg
+    column : Int    # column (field) number this argument applies to
+    cond : Int      # condition (equal, less than, etc)
+    value : Int     # encoded value
+  end
+
+  fun make_query = wg_make_query(db : Void*, matchrec : Void*, reclen : Int, arglist : QueryArg*, argc : Int) : Query
+  fun fetch = wg_fetch(db : Void*, query : Query) : Void*
+  fun free_query = wg_free_query(db : Void*, query : Query) : Void
+  
+  fun encode_query_param_null = wg_encode_query_param_null(db : Void*, data : String) : Int
+  fun encode_query_param_record = wg_encode_query_param_record(db : Void*, data : Void*) : Int
+  fun encode_query_param_char = wg_encode_query_param_char(db : Void*, data : Char) : Int
+  fun encode_query_param_fixpoint = wg_encode_query_param_fixpoint(db : Void*, data : Double) : Int
+  fun encode_query_param_date = wg_encode_query_param_date(db : Void*, data : Int) : Int
+  fun encode_query_param_time = wg_encode_query_param_time(db : Void*, data : Int) : Int
+  fun encode_query_param_var = wg_encode_query_param_var(db : Void*, data : Int) : Int
+  fun encode_query_param_int = wg_encode_query_param_int(db : Void*, data : Int) : Int
+  fun encode_query_param_double = wg_encode_query_param_double(db : Void*, data : Double) : Int
+  fun encode_query_param_str = wg_encode_query_param_str(db : Void*, data : String, lang : String) : Int
+  fun encode_query_param_xmlliteral = wg_encode_query_param_xmlliteral(db : Void*, data : String, xdtype : String) : Int
+  fun encode_query_param_uri = wg_encode_query_param_uri(db : Void*, data : String, prefix : String) : Int
+  fun free_query_param = wg_free_query_param(db : Void*, data : Int) : Int
+
+  # Query "arglist" parameters
+  enum COND
+    EQUAL     = 1
+    NOT_EQUAL = 1 << 1
+    LESSTHAN  = 1 << 2
+    GREATER   = 1 << 3
+    LTEQUAL   = 1 << 4
+    GTEQUAL   = 1 << 5 
+  end
+
+  # Simplified query functions
   fun find_record = wg_find_record(db : Void*, fieldnr : Int, cond : Int, data : Int, lastrecord : Void*) : Void*
   fun find_record_null = wg_find_record_null(db : Void*, fieldnr : Int, cond : Int, data : String, lastrecord : Void*) : Void*
   fun find_record_record = wg_find_record_record(db : Void*, fieldnr : Int, cond : Int, data : Void*, lastrecord : Void*) : Void*
@@ -190,23 +232,4 @@ lib WhiteDB
   fun find_record_str = wg_find_record_str(db : Void*, fieldnr : Int, cond : Int, data : String, lastrecord : Void*) : Void*
   fun find_record_xmlliteral = wg_find_record_xmlliteral(db : Void*, fieldnr : Int, cond : Int, data : String, xsdtype : String, lastrecord : Void*) : Void*
   fun find_record_uri = wg_find_record_uri(db : Void*, fieldnr : Int, cond : Int, data : String, prefix : String, lastrecord : Void*) : Void*
-
-  # ...
-  
-
-  fun database_freesize = wg_database_freesize(db : Void*) : Int
-  fun database_size = wg_database_size(db : Void*) : Int
-
-  
-  # ...
-
-  # Query "arglist" parameters
-  enum COND
-    EQUAL     = 1
-    NOT_EQUAL = 1 << 1
-    LESSTHAN  = 1 << 2
-    GREATER   = 1 << 3
-    LTEQUAL   = 1 << 4
-    GTEQUAL   = 1 << 5 
-  end
 end
